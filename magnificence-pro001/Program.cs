e7632e9ff905426a7a58b1d5a61874cd4282416a
magnificence <39826801+magnificence-dream@users.noreply.github.com>
2019-05-02 18:11:39 +0800

    init
    init

---
+++ magnificence-pro001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace magnificence_pro001
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string c = null;
+			if (c != null)
+			{
+				Console.WriteLine("不可能执行的代码.");
+			}
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
